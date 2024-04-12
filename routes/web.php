81e16f6c62b2ec29b59c931a7249520dbec7a774
Dev02-at-013708807864 <edilson.santos@kasi.com.br-at-178825947031>
2024-04-12 16:28:27 -0300

    Feat Laravel: Subindo projeto

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/cadastro', function () {
     return view('cadastro');
