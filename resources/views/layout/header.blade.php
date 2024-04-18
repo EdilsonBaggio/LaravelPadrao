63360eb687fa5a83b909a7a2509a0c1183236f5c
Dev02-at-013708807864 <edilson.santos@kasi.com.br-at-178825947031>
2024-04-18 15:16:05 -0300

    Feat Arquivos: Ajustando arquivos

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,13 +1,15 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="">Cadastro</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('login')}}">Login</a>
+          <a class="nav-link active" href="{{route('cadastro')}}">Cadastro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="{{route('login')}}">Login</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('listar')}}">Listar usuario</a>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,18 +11,17 @@
                   @csrf 
 
                   <div class="form-group"> 
-                     <label for="Usuário:">Usuário:</label> 
-                     <input id="Usuário:" type="text" class="form-control @error('Usuário:') is-invalid @enderror" name="Usuário:" value="{{ old('Usuário:') }}" required autofocus> 
+                     <label class="mb-2" for="usuario">usuario</label> 
+                     <input id="usuario" type="text" class="form-control @error('usuario') is-invalid @enderror" name="usuario" value="{{ old('usuario') }}" required autofocus> 
                   </div>
                   @error('Usuário:') 
                   <span class="invalid-feedback" role="alert"> 
                      <strong>{{ $message }}</strong> 
                   </span>
                   @enderror 
-
-
+                  <br>
                   <div class="form-group">
-                     <label for="password">Senha:</label>
+                     <label class="mb-2" for="password">Senha:</label>
                      <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
                   </div>
                   @error('password')
@@ -31,7 +30,6 @@
                   </span>
                   @enderror
                   <br>
-                  </br>
                   <div class="form-group mb-0 botoes-cadastro">
                      <button type="submit" class="btn btn-primary"> 
                      Entrar
