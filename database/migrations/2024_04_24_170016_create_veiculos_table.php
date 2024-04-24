96912346ca6b80e8dea2394999363f62cf65ec17
unknown <agathapolati1@gmail.com>
2024-04-24 15:02:24 -0300

    Cadastro Veiculo

--- database/migrations/2024_04_24_170016_create_veiculos_table.php
+++ database/migrations/2024_04_24_170016_create_veiculos_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('veiculos', function (Blueprint $table) {
             $table->id();
+            $table->string('placa');
+            $table->string('modelo');
+            $table->string('cor');
+            $table->string('marca');
             $table->timestamps();
         });
     }
