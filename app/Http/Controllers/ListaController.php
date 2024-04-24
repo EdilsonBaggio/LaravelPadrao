e52cbeb89f6865f5ddae74264c7c071609671922
Dev02-at-013708807864 <edilson.santos@kasi.com.br-at-178825947031>
2024-04-24 16:26:37 -0300

    Feat Model: Ajustando nome do Model com CamelCase

--- app/Http/Controllers/ListaController.php
+++ app/Http/Controllers/ListaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
-use App\Models\lista; // Certifique-se de importar o modelo Pessoa
+use App\Models\Lista; // Certifique-se de importar o modelo Pessoa
 
 class ListaController extends Controller
 {
@@ -12,7 +12,7 @@ class ListaController extends Controller
     {
         $columns = Schema::getColumnListing('pessoas');
 
-        $pessoas = lista::all(); // Supondo que você tenha um modelo Pessoa
+        $pessoas = Lista::all(); // Supondo que você tenha um modelo Pessoa
 
         return view('listar-usuario', compact('columns', 'pessoas'));
     }
--- app/Models/Lista.php
+++ app/Models/Lista.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class lista extends Model
+class Lista extends Model
 {
     protected $table = 'pessoas';
 }
