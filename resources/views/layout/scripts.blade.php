d5d8b50924fe588b410bfe73b434c817a4e4f918
Dev02-at-013708807864 <edilson.santos@kasi.com.br-at-178825947031>
2024-04-24 11:14:38 -0300

    Feat Editar: Ediçao de cadastro de usuário.

--- resources/views/layout/scripts.blade.php
+++ resources/views/layout/scripts.blade.php
@@ -14,6 +14,7 @@
     crossorigin="anonymous"></script>
 
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
     AOS.init();
