2e6bf3072a68c22648c0ea5bd9449a724937b892
Edilson Santos <edilson.santos@kasi.com.br-at-178825947031>
2024-09-18 15:59:55 -0300

    Feat Tabelas: Ajustando o retorno dos dados nas tabelas

--- app/Livewire/ListarVeiculos.php
+++ app/Livewire/ListarVeiculos.php
@@ -3,7 +3,7 @@
 namespace App\Livewire; //define o namespace do componente Livewire
 
 use Livewire\Component;
-use App\Models\Veiculos;
+use App\Models\ListarVeiculos as Veiculos;
 use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,16 +27,15 @@ class ListarVeiculos extends Component
 
     public function render()
     {
-
         $userId = Auth::user()->id;
         $veiculos = Veiculos::leftJoin('pessoas', 'pessoas.id', '=', 'veiculos.usuario_id') 
-                     ->leftJoin('garagens', 'garagens.id', '=', 'veiculos.garagem_id')
-                     ->select('veiculos.*', 'pessoas.email as email_usuario', 'pessoas.name as usuario','garagens.nome as nome')
-                     ->where('garagem_id', $userId)
-                     ->whereNull('veiculos.deleted_at')
-                     ->paginate(10);
+            ->leftJoin('garagens', 'garagens.id', '=', 'veiculos.usuario_id')
+            ->select('veiculos.*', 'pessoas.email as email_usuario', 'pessoas.name as usuario', 'garagens.nome as nome')
+            ->where('veiculos.usuario_id', $userId)
+            ->whereNull('veiculos.deleted_at')
+            ->paginate(10);
+            
         return view('livewire.listar-veiculos', compact('veiculos'));
     }
-
 
 }
