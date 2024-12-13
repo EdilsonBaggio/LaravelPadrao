7bb2461db0cc37b624c2b56ffdc6ed308c9c6c2f
Edilson Santos <edilson.santos@kasi.com.br-at-178825947031>
2024-12-13 10:09:56 -0300

    Feat Home: Adicionado tela inicial ao efetuar o ligin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,16 @@
 @section('content')
 <div class="row gx-0 mt-5 justify-content-center">
     <div class="col-2">
-        <a class="p-4 border bg-light" href="{{ route('listar') }}" target="_blank">Meus dados</a>
+        <a class="p-4 border bg-light" href="{{ route('listar') }}">Meus dados</a>
     </div>
     <div class="col-2">
-        <a class="p-4 border bg-light" href="{{route('cadastro-garagem')}}" target="_blank">Cadastrar Garagem</a>
+        <a class="p-4 border bg-light" href="{{route('cadastro-garagem')}}">Cadastrar Garagem</a>
     </div>
     <div class="col-2">
-        <a class="p-4 border bg-light" href="{{route('listar-garagem')}}" target="_blank">Listar Garagens</a>
+        <a class="p-4 border bg-light" href="{{route('listar-garagem')}}">Listar Garagens</a>
     </div>
     <div class="col-2">
-        <a class="p-4 border bg-light" href="{{route('cadastro-veiculo')}}" target="_blank">Cadastrar Veiculos</a>
+        <a class="p-4 border bg-light" href="{{route('cadastro-veiculo')}}">Cadastrar Veiculos</a>
     </div>
 </div>
 @endsection
